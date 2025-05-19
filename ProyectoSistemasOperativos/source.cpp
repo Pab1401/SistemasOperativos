df3e4ccf8c6b7b58567f40311270033edaa437cb
Pab1401 <pab140.ssb@gmail.com>
2025-05-19 10:50:32 -0600

    V 0.1.2

--- ProyectoSistemasOperativos/source.cpp
+++ ProyectoSistemasOperativos/source.cpp
@@ -17,16 +17,31 @@ int main()
 
 }
 
-void FCFS()
+void FIFO()
 {
 	queue<Program> programList;
 	int input = 1;
+	bool completed = false;
 	do
 	{
 		programList.push(PrgCreate());
 		cout << "Quieres agregar otro programa? \n1. Si \n0. No" << endl;
 		cin >> input;
 	} while (input != 0);
+	int counter;
+	while (!completed)
+	{
+		Program current;
+		if (programList.front().llegada =< counter)
+		current = programList.front();
+		do
+		{
+			current.rafagas--;
+		} while (current.rafagas > 0);
+		programList.pop();
+		if (programList.empty())
+			completed = true;
+	}
 }
 
 Program PrgCreate()
